e4d04ea4daeb73afaf74169bb0f477b908861281
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-04-21 11:29:21 -0300

    Update: changed import texts

--- app_seu_madeu_sucos/lib/main.dart
+++ app_seu_madeu_sucos/lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:app_seu_madeu_sucos/View/Logic/Bloc/Authenticate/AuthBloc.dart';
-import 'package:app_seu_madeu_sucos/View/Logic/Bloc/Authenticate/AuthState.dart';
-import 'package:app_seu_madeu_sucos/View/Widgets/Login/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'View/Logic/Bloc/Authenticate/AuthBloc.dart';
+import 'View/Logic/Bloc/Authenticate/AuthState.dart';
+import 'View/Widgets/Login/LoginPage.dart';
 import 'View/Widgets/HomePage.dart';
 
 void main() {
