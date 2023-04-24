ecc42e340bb4fbc7b4b48ecd2b9aa3f344102b55
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-04-24 15:19:12 -0300

    Update product screen and addded snackbar to tell a product has been added to the cart

--- app_seu_madeu_sucos/lib/View/Widgets/ProductScreen.dart
+++ app_seu_madeu_sucos/lib/View/Widgets/ProductScreen.dart
@@ -13,34 +13,50 @@ class _ProductScreenState extends State<ProductScreen> {
     return ListView.builder(
         itemCount: 15,
         itemBuilder: (BuildContext context, int index) => Card(
-          margin: const EdgeInsets.all(7),
-          elevation: 5,
-          color: Colors.green,
-          child: ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image.asset(
-                'lib/Assets/seuMadeu.jpeg',
-              ),
-            ),
-            title: Text("Produto $index"),
-            subtitle: const Text("Descrição do produto"),
-            trailing: Container(
-              padding: const EdgeInsets.all(10),
-              color: Colors.orange,
-              child: GestureDetector(
-                onTap: (){
-                  //TODO: Add product to cart
+              margin: const EdgeInsets.all(7),
+              elevation: 5,
+              color: Colors.green,
+              child: ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    'lib/Assets/seuMadeu.jpeg',
+                  ),
+                ),
+                title: Text("Produto $index"),
+                subtitle: const Text("Descrição do produto"),
+                trailing: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green.shade800,
+                      padding: EdgeInsets.all(17),
+                      shape: CircleBorder(),
+                    ),
+                    onPressed: () {
+                      //TODO: Adicionar produto ao carrinho
+                      ScaffoldMessenger.of(context).showSnackBar(addedToCartSnackbar(index));
+                    },
+                    child: const Icon(
+                      Icons.add_shopping_cart_sharp,
+                      size: 22,
+                    )),
+                contentPadding: const EdgeInsets.all(15),
+                onTap: () {
+                  //TODO: Open single product screen
                 },
-                child: const Icon(Icons.add_shopping_cart_sharp,)
               ),
-            ),
-            contentPadding: const EdgeInsets.all(15),
-            onTap: (){
-              //TODO: Open single product screen
-            },
-          ),
-        )
-      );
+            ));
+  }
+
+  SnackBar addedToCartSnackbar(int productIndex) {
+    return SnackBar(
+      duration: const Duration(seconds: 2),
+      content: Text("Produto $productIndex adicionado ao carrinho!"),
+      action: SnackBarAction(
+        label: "Desfazer", 
+        onPressed: (){
+          //TODO: Desfazer ação de adicionar
+        }
+      ),
+    );
   }
 }
