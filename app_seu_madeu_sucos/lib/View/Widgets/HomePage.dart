e14c54694990233203d548430e1cae5e71d4edb8
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-04-24 14:51:09 -0300

    Create product screen ui

--- app_seu_madeu_sucos/lib/View/Widgets/HomePage.dart
+++ app_seu_madeu_sucos/lib/View/Widgets/HomePage.dart
@@ -20,65 +20,78 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const UserAccountsDrawerHeader(
-              accountName: Text("Nome Completo do Usuário"),
-              accountEmail: Text(
-                "Email completo do usuário",
-                style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
-              ),
-              currentAccountPicture: CircleAvatar(),
-            ),
-            ListTile(
-              title: const Text("Perfil"),
-              textColor: Colors.green,
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const UserProfileScreen()));
-              },
-            ),
-            ListTile(
-              title: const Text("Planos"),
-              textColor: Colors.green,
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const UserPlanScreen()));
-              },
-            ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        drawer: drawer(),
+        body: IndexedStack(
+          index: currentScreen,
+          children: const [
+            HistoryPage(),
+            ProductScreen(),
+            CartPage(),
           ],
         ),
-      ),
-      body: IndexedStack(
-        index: currentScreen,
-        children: const [
-          HistoryPage(),
-          CartPage(),
-          ProductScreen(),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (int value) {
-          setState(() {
-            currentScreen = value;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.history_outlined), label: "Histórico"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag_outlined),
-            label: "Pedido",
+        bottomNavigationBar: bottomNavigationBar());
+  }
+
+  Widget bottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: (int value) {
+        setState(() {
+          currentScreen = value;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.history_outlined), label: "Histórico"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_bag_outlined),
+          label: "Pedido",
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined), label: "Carrinho"),
+      ],
+      currentIndex: currentScreen,
+      fixedColor: Colors.green,
+    );
+  }
+
+  Widget drawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          const UserAccountsDrawerHeader(
+            accountName: Text("Nome Completo do Usuário"),
+            accountEmail: Text(
+              "Email completo do usuário",
+              style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
+            ),
+            currentAccountPicture: CircleAvatar(),
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart_outlined), label: "Carrinho"),
+          ListTile(
+            title: const Text("Perfil"),
+            textColor: Colors.green,
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const UserProfileScreen()));
+            },
+          ),
+          ListTile(
+            title: const Text("Planos"),
+            textColor: Colors.green,
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const UserPlanScreen()));
+            },
+          ),
         ],
-        currentIndex: currentScreen,
-        fixedColor: Colors.green,
       ),
     );
   }
--- app_seu_madeu_sucos/lib/View/Widgets/ProductScreen.dart
+++ app_seu_madeu_sucos/lib/View/Widgets/ProductScreen.dart
@@ -10,6 +10,37 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Tela de produtos");
+    return ListView.builder(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) => Card(
+          margin: const EdgeInsets.all(7),
+          elevation: 5,
+          color: Colors.green,
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Image.asset(
+                'lib/Assets/seuMadeu.jpeg',
+              ),
+            ),
+            title: Text("Produto $index"),
+            subtitle: const Text("Descrição do produto"),
+            trailing: Container(
+              padding: const EdgeInsets.all(10),
+              color: Colors.orange,
+              child: GestureDetector(
+                onTap: (){
+                  //TODO: Add product to cart
+                },
+                child: const Icon(Icons.add_shopping_cart_sharp,)
+              ),
+            ),
+            contentPadding: const EdgeInsets.all(15),
+            onTap: (){
+              //TODO: Open single product screen
+            },
+          ),
+        )
+      );
   }
 }
