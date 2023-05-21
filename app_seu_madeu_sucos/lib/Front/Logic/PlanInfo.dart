c0a8e11e83aa3ae40037ebb61084f83e226d6592
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-05-21 11:02:23 -0300

    Create: implemented mocked plan data for front

---
+++ app_seu_madeu_sucos/lib/Front/Logic/PlanInfo.dart
@@ -0,0 +1,33 @@
+import '../Entities/Plan.dart';
+
+class PlanInfo {
+  //Create singleton
+  static final PlanInfo _instance = PlanInfo._internal();
+  factory PlanInfo() => _instance;
+  PlanInfo._internal();
+
+  //Create list of plans
+  static List<Plan> allPlans = [
+    Plan(
+      id: 0,
+      name: "Plano Padrão",
+      description: "Plano com 2 sucos normais por semana",
+      value: 110.00,
+      lastModified: DateTime.now(),
+    ),
+    Plan(
+      id: 1,
+      name: "Plano Especial",
+      description: "Plano com 2 sucos com mistura por semana",
+      value: 130.00,
+      lastModified: DateTime.now(),
+    ),
+    Plan(
+      id: 2,
+      name: "Plano Premium",
+      description: "Plano com 2 sucos com mistura especial por semana",
+      value: 160.00,
+      lastModified: DateTime.now()
+    ),
+  ];
+}
