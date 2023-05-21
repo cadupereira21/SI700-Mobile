cec35ced0c5ac4c369d2b9c4472001e3a1e415ce
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-05-21 12:24:28 -0300

    Create: implemented user plan screen

--- app_seu_madeu_sucos/lib/Front/Logic/PlanInfo.dart
+++ app_seu_madeu_sucos/lib/Front/Logic/PlanInfo.dart
@@ -11,21 +11,21 @@ class PlanInfo {
     Plan(
       id: 0,
       name: "Plano Padrão",
-      description: "Plano com 2 sucos normais por semana",
+      description: "Plano com 2 sucos normais por semana!",
       value: 110.00,
       lastModified: DateTime.now(),
     ),
     Plan(
       id: 1,
       name: "Plano Especial",
-      description: "Plano com 2 sucos com mistura por semana",
+      description: "Plano com 2 sucos com mistura por semana!",
       value: 130.00,
       lastModified: DateTime.now(),
     ),
     Plan(
       id: 2,
       name: "Plano Premium",
-      description: "Plano com 2 sucos com mistura especial por semana",
+      description: "Plano com 2 sucos com mistura especial por semana!",
       value: 160.00,
       lastModified: DateTime.now()
     ),
--- app_seu_madeu_sucos/lib/Front/View/Widgets/UserPlanScreen.dart
+++ app_seu_madeu_sucos/lib/Front/View/Widgets/UserPlanScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../Entities/Plan.dart';
+import '../../Logic/PlanInfo.dart';
+
 class UserPlanScreen extends StatefulWidget {
   const UserPlanScreen({super.key});
 
@@ -8,17 +11,96 @@ class UserPlanScreen extends StatefulWidget {
 }
 
 class _UserPlanScreenState extends State<UserPlanScreen> {
+  List<Plan> allPlans = PlanInfo.allPlans;
+
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(50.0),
-      child: Card(
-        child: Column(
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.77,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              color: Colors.white,
+            ),
+            child: 
+            ListView.builder(
+            itemCount: allPlans.length,
+            itemBuilder: (BuildContext context, int index) =>
+              planTile(allPlans[index]),
+            ),
+          ),
+          Expanded(   
+            child: 
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                color: Colors.white,
+              ),
+              //color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width*0.5, 15, 10, 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Voltar"),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget planTile(Plan plan) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      //elevation: 2,
+      color: Colors.white,
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: Colors.green, width: 2),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        title: Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                plan.name, 
+                style: const TextStyle(fontSize: 20, color: Colors.green),
+              ),
+              Text(
+                "R\$ ${plan.value.toStringAsFixed(2)}", 
+                style: const TextStyle(fontSize: 20, color: Colors.orange),
+              ),
+            ],
+          )
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Tela de planos da loja"),
-            ElevatedButton(onPressed: (){Navigator.pop(context);}, child: const Text("Voltar"))
+            Text(plan.description, style: const TextStyle(fontSize: 15, color: Colors.black)),
           ],
         ),
+        //trailing: addToCartButton(product),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 10),
       ),
     );
   }
