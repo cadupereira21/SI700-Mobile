0d5b5be5f8ba8de1742c0d8db14b70846e9e0c33
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-05-26 16:28:40 -0300

    Create: implemented getAllProducts on ProductsService and call it on bloc for testing

--- app_seu_madeu_sucos/lib/Controller/Requester/ProductRequester/ProductRequesterBloc.dart
+++ app_seu_madeu_sucos/lib/Controller/Requester/ProductRequester/ProductRequesterBloc.dart
@@ -1,21 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../Service/ProductServiceImp.dart';
 import '../RequestState.dart';
 import 'ProductRequesterEvent.dart';
 
 class ProductRequesterBloc extends Bloc<ProductRequesterEvent, RequestState> {
-  //declare service
+  ProductServiceImp service = ProductServiceImp.instance;
   ProductRequesterBloc(super.initialState) {
     on<GetAllProductsRequest>(
       (event, emit) => _getAllProducts(event, emit),
     );
   }
 
-  _getAllProducts(GetAllProductsRequest event, Emitter<RequestState> emit) async {
+  _getAllProducts(
+      GetAllProductsRequest event, Emitter<RequestState> emit) async {
     emit(ProcessingRequest());
 
     try {
-      //await service.getAllProducts();
+      await service.getAllProducts();
       emit(RequestSuccess(message: "Produtos encontrados com sucesso!"));
       print((state as RequestSuccess).message);
     } catch (e) {
--- app_seu_madeu_sucos/lib/Service/ProductServiceImp.dart
+++ app_seu_madeu_sucos/lib/Service/ProductServiceImp.dart
@@ -1,7 +1,20 @@
-import 'dart:developer';
-
+import '../Model/Product.dart';
 import 'RequestStatus.dart';
+import 'Service.dart';
 
-class ProductServiceImp extends Service{
-  
+class ProductServiceImp extends Service {
+  static const String REQ_TITLE_GET_PRODUCT = "Get User Request";
+
+  static final ProductServiceImp instance = ProductServiceImp._internal();
+  ProductServiceImp._internal();
+
+  Future<List<Product>?> getAllProducts() async {
+    List<Product> productColletion = [];
+    final response = await dio.get("$baseUrl}/products.json");
+
+    print(response.data.toString());
+    //Put products on list
+    //Notify
+    return null;
+  }
 }
