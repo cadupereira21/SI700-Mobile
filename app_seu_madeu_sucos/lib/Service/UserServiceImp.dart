73e7b60320961b2944b69eec21257d2b678a5c05
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-05-26 07:43:59 -0300

    Create: implemented stream notifier on UserServiceImp.dart

--- app_seu_madeu_sucos/lib/Service/UserServiceImp.dart
+++ app_seu_madeu_sucos/lib/Service/UserServiceImp.dart
@@ -1,31 +1,48 @@
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:app_seu_madeu_sucos/Controller/Requester/RequestState.dart';
 import 'package:dio/dio.dart';
 
+import '../Interface/Notifier.dart';
 import '../Model/User.dart';
+import 'RequestStatus.dart';
 
-class UserServiceImp {
+class UserServiceImp implements Notifier {
+  final String _REQ_TITLE_CREATE_USER = "Create User Request";
+
   final String _baseUrl = "https://seu-madeu-sucos-default-rtdb.firebaseio.com";
-  final dio = Dio();
+  final _dio = Dio();
 
   static final UserServiceImp instance = UserServiceImp._internal();
   UserServiceImp._internal();
+  StreamController? _streamController;
 
-  Future<bool> createUser(User user) async {
-    final response = await dio.post("${_baseUrl}/users.json", data: json.encode({
-      "email": user.email,
-      "password": user.password,
-      "Client":{
-        "name": user.client!.name,
-        "address": user.client!.address,
-        "phone": user.client!.phone,
-      },
-      "activePlan": null
-    }));
-    if (response.statusCode == 200) {
-      return true;
-    } else {
-      throw Exception("Erro ao criar usuário!");
-    }
+  Future<void> createUser(User user) async {
+    final response = await _dio.post("${_baseUrl}/users.json",
+        data: json.encode({
+          "email": user.email,
+          "password": user.password,
+          "client": {
+            "name": user.client!.name,
+            "address": user.client!.address,
+            "phone": user.client!.phone,
+          },
+          "activePlan": null
+        })); 
+    notify(requestTitle: _REQ_TITLE_CREATE_USER, responseStatus: RequestStatus.SUCCESSFUL);
+  }
+
+  @override
+  void notify(
+      {String? requestTitle, RequestStatus? responseStatus, List<Object>? object}) {
+    _streamController?.sink.add([requestTitle, responseStatus, object]);
+  }
+
+  @override
+  // TODO: implement stream
+  Stream get stream {
+    _streamController ??= StreamController.broadcast();
+    return _streamController!.stream;
   }
 }
