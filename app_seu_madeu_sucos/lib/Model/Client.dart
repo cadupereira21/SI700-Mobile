da3fd8bdb96c02e9f2829fa0e20cc7ffa3aa1962
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-06-21 11:15:16 -0300

    added getters and setters in all model classes

--- app_seu_madeu_sucos/lib/Model/Client.dart
+++ app_seu_madeu_sucos/lib/Model/Client.dart
@@ -16,6 +16,16 @@ class Client {
     this.activePlan,
   });
 
+  //create getters and setters
+  String? get getName => name;
+  set setName(String? name) => this.name = name;
+  Address? get getAddress => address;
+  set setAddress(Address? address) => this.address = address;
+  String? get getPhone => phone;
+  set setPhone(String? phone) => this.phone = phone;
+  ActivePlan? get getActivePlan => activePlan;
+  set setActivePlan(ActivePlan? activePlan) => this.activePlan = activePlan;
+
   //Create to string
   @override
   String toString() {
