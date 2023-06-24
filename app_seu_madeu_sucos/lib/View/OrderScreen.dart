7560ad4d6c699f9a85c126145bd1d5fcf52d77df
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-06-24 12:15:41 -0300

    Change drawer and add appBar

--- app_seu_madeu_sucos/lib/View/OrderScreen.dart
+++ app_seu_madeu_sucos/lib/View/OrderScreen.dart
@@ -10,55 +10,63 @@ class OrderScreen extends StatefulWidget {
 class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.white,
-        child: screenScrolablleContent(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Finalizar Pedido"),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
+      body: screenScrolablleContent(),
     );
   }
 
-  Widget screenScrolablleContent(){
-    return Scaffold(
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0, bottom: 0.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween, 
-                    children:[
-                      Text("Product List View"),
-                      Text("Checkers and Dropdowns"),
-                    ],
-                  ),
-                  Text("Comments"),
-                  Text("Address Form"),
-                ],
-              ),
-            ),
-          ),
-          Expanded(child: Align(
-            alignment: Alignment.bottomCenter, 
+  Widget screenScrolablleContent() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 15.0, left: 15.0, right: 15.0, bottom: 0.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text("Total: R\$100,00"),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ElevatedButton(onPressed: (){}, child: Text("Enviar")),
-                    ElevatedButton(onPressed: (){}, child: Text("Cancelar")),                    
+                    Text("Product List View"),
+                    Text("Checkers and Dropdowns"),
                   ],
                 ),
+                Text("Comments"),
+                Text("Address Form"),
               ],
             ),
-          )),
-        ],
-      ),
+          ),
+        ),
+        Expanded(
+            child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text("Total: R\$100,00"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("Enviar")),
+                  ElevatedButton(onPressed: () {}, child: Text("Cancelar")),
+                ],
+              ),
+            ],
+          ),
+        )),
+      ],
     );
   }
 }
