44f792ab7312fe390ecea7043d14c8d59c000c21
Carlos Eduardo A Pereira <carloseduardo2101@gmail.com>
2023-06-24 12:06:13 -0300

    Implemented basic order screen layout

--- app_seu_madeu_sucos/lib/View/OrderScreen.dart
+++ app_seu_madeu_sucos/lib/View/OrderScreen.dart
@@ -13,7 +13,51 @@ class _OrderScreenState extends State<OrderScreen> {
     return Expanded(
       child: Container(
         color: Colors.white,
-        child: Text("Order Screen"),
+        child: screenScrolablleContent(),
+      ),
+    );
+  }
+
+  Widget screenScrolablleContent(){
+    return Scaffold(
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0, bottom: 0.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+                    children:[
+                      Text("Product List View"),
+                      Text("Checkers and Dropdowns"),
+                    ],
+                  ),
+                  Text("Comments"),
+                  Text("Address Form"),
+                ],
+              ),
+            ),
+          ),
+          Expanded(child: Align(
+            alignment: Alignment.bottomCenter, 
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("Total: R\$100,00"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(onPressed: (){}, child: Text("Enviar")),
+                    ElevatedButton(onPressed: (){}, child: Text("Cancelar")),                    
+                  ],
+                ),
+              ],
+            ),
+          )),
+        ],
       ),
     );
   }
